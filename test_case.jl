6e4562f9d54dec4b9d204ab1bb6f86b5f8efed59
Temi <oshinowo@princeton.edu>
2019-10-04 15:36:19 -0400

    Commit part 1.A.b

--- test_case.jl
+++ test_case.jl
@@ -1,1 +1,12 @@
 #We've written something here
+
+#we staged this file
+#red= things gone
+#green = new stuff
+
+#need to add a commit message before you committ
+
+#need to push before it goes into the repository
+
+
+#Change #3
